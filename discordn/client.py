c33e480ca018915ec13aa930882b705af808bee9
Natalie Fearnley <nfearnley@gmail.com>
2021-08-29 02:51:42 -0400

    Added on_error handling

--- discordn/client.py
+++ discordn/client.py
@@ -1,5 +1,12 @@
+import sys
+import logging
+import traceback
+
 import discord.utils
 from discord import Client
+from discord.ext import commands
+
+logger = logging.getLogger("discordn")
 
 
 def oauth_url(self, *args, **kwargs):
@@ -18,6 +25,39 @@ def activity_getter(self):
 activity = property(fget=activity_getter, fset=Client.activity.fset)
 
 
+def formatTraceback(err) -> str:
+    return "".join(traceback.format_exception(type(err), err, err.__traceback__))
+
+
+async def on_error(self, event, *args, **kwargs):
+    """|coro|
+
+    The default error handler provided by the client.
+
+    By default this prints to :data:`sys.stderr` however it could be
+    overridden to have a different implementation.
+    Check :func:`~discord.on_error` for more details.
+    """
+    _, error, _ = sys.exc_info()
+    # Get actual error
+    err = getattr(error, "original", error)
+    # DigiException handling
+    if isinstance(err, commands.FancyException):
+        message = err.formatMessage()
+        if message is not None:
+            logger.log(err.level, message)
+            logger.error(formatTraceback(error))
+    elif isinstance(err, commands.FancyContextException):
+        message = str(err)
+        if message is not None:
+            logger.log(err.level, message)
+            logger.error(formatTraceback(error))
+    else:
+        logger.error(f"Ignoring exception in {event}")
+        logger.error(formatTraceback(error))
+
+
 def patch():
     Client.oauth_url = oauth_url
     Client.activity = activity
+    Client.on_error = on_error
